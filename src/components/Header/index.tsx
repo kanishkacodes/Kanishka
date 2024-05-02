23ff6acbfd2b490a4019eaed53d203e3849273db
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-05-02 14:01:06 +0530

    test 2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,35 @@
-'use client'
-import styles from './style.module.scss'
-
-import { useEffect, useState } from 'react';
-import Nav from './nav';
-import { AnimatePresence } from 'framer-motion';
+'use client';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import styles from './style.module.scss';
 import { usePathname } from 'next/navigation';
+import { AnimatePresence } from 'framer-motion';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Nav from './nav';
+import gsap from 'gsap';
 
-export default function Home() {
+export default function Index() {
+    const header = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+    const pathname = usePathname();
+    const button = useRef(null);
 
-  const [isActive, setIsActive] = useState(false);
-  const pathname = usePathname();
+    useEffect( () => {
+      if(isActive) setIsActive(false)
+    }, [pathname])
 
-  useEffect( () => {
-    if(isActive) setIsActive(false)
-  }, [pathname])
+    
 
-  return (
-    <>
-    <div className={styles.main}>
-
-      <div className={styles.header}>
-        <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
-          <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+    return (
+        <>
+        
+        <div ref={button} className={styles.headerButtonContainer}>
+            <div onClick={() => {setIsActive(!isActive)}} className={`${styles.button}`}>
+                <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+            </div>
         </div>
-      </div>
-
-    </div>
-    <AnimatePresence mode="wait">
-      {isActive && <Nav />}
-    </AnimatePresence>
-    </>
-  )
+        <AnimatePresence mode="wait">
+            {isActive && <Nav />}
+        </AnimatePresence>
+        </>
+    )
 }
